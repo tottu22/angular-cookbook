ea3287918765492c5d35929b01386cea491186cf
tottu <tomoya.tozuka@gmail.com>
2019-02-11 18:04:08 +0900

    First commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule} from '@angular/core'
+import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router'
+import {RecipeListComponent} from './recipe-list/recipe-list.component';
+import {RecipeDataComponent} from './recipe-data/recipe-data.component';
 
-const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    RouterModule.forRoot([
+      {path:"", redirectTo: "/recipe-list", pathMatch: 'full'},
+      {path:"recipe-list", component:RecipeListComponent},
+      {path:"recipe-data/:id", component:RecipeDataComponent}
+    ])
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations:[]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDataComponent } from './recipe-data/recipe-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDataComponent
   ],
   imports: [
     BrowserModule,
